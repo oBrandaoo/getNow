2baaa0c00adec86fe24ec29e0d1c3df44fdd0739
Rafael Brandão <rafaelbrandaoreis@gmail.com.br>
2025-04-05 14:00:29 -0300

    feat: add initial for products

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
+import Products from './pages/products';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </Router>
   );
