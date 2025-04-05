7716c54df753736c323564e518370110e429da41
Rafael Brandão <rafaelbrandaoreis@gmail.com.br>
2025-04-05 03:03:39 -0300

    feat: add front end and simple route

--- src/main/java/com/ideia/sw/GetNow/controller/mainController.java
+++ src/main/java/com/ideia/sw/GetNow/controller/mainController.java
@@ -1,7 +1,15 @@
 package com.ideia.sw.GetNow.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
 public class mainController {
     
+    @GetMapping("/hello")
+    public String index() {
+        return "Hello World!";
+    }
 }
