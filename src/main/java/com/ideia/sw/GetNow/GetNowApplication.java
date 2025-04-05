1d890848fa4fc393f0979b978aa49d9ea701697e
Rafael Brandão <rafaelbrandaoreis@gmail.com.br>
2025-04-05 02:02:26 -0300

    Initial commit with Spring Boot project

---
+++ src/main/java/com/ideia/sw/GetNow/GetNowApplication.java
@@ -0,0 +1,13 @@
+package com.ideia.sw.GetNow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ideia/sw/GetNow/GetNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideia.sw.GetNow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
