486dd01f11e3238fab743116164c3c9ee15813d8
Rafael Brandão <rafaelbrandaoreis@gmail.com.br>
2025-04-05 14:19:24 -0300

    fix: fix bug to run application

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
-import Products from './pages/products';
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/products" element={<Products />} />
       </Routes>
     </Router>
   );
